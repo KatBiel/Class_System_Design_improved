4fa1e2c8498360133da8fd4a84da43f303a6981c
Kat <katarzynaa89@gmail.com>
2023-11-22 09:16:25 +0000

    Final commit

--- lib/readable_entry_extractor.py
+++ lib/readable_entry_extractor.py
@@ -4,9 +4,19 @@ class ReadableEntryExtractor():
         self.diary = diary
     
     def extract(self, wpm, time):
+        readable_entries = [
+            entry for entry in self.diary.all()
+            if self._entry_readable_in_time(wpm, time, entry)
+        ]
+        if len(readable_entries) == 0:
+                return None
+        else:
+            return max(readable_entries,
+                        key = lambda entry: self.count_words(entry.content))
+
+    def _entry_readable_in_time(self, wpm, time, entry):
         length_of_readble_chunk = wpm * time
-        readable_chunk = []
-        for entry in self.diary.all():
-            if len(entry.content) <= length_of_readble_chunk:
-                readable_chunk.append(entry.content)
-        return readable_chunk
+        return self.count_words(entry.content) <= length_of_readble_chunk
+
+    def count_words(self, string):
+        return len(string.split(" "))
--- tests/test_readable_entry_extractor_integration.py
+++ tests/test_readable_entry_extractor_integration.py
@@ -1,34 +1,49 @@
-# from lib.diary import *
-# from lib.diary_entry import *
-# from lib.readable_entry_extractor import *
+from lib.diary import *
+from lib.diary_entry import *
+from lib.readable_entry_extractor import *
 
-# '''When add multiple diary entries ReadableEntryExtractor extracts
-# and returns a the longest readable'''
-# def test_extracts_longest_multiple_enties():
-#     diary = Diary()
-#     entry1 = DiaryEntry("My title 1", "One two three four five")
-#     entry2 = DiaryEntry("My title 2", "six seven eight")
-#     entry3 = DiaryEntry("My title 3", "nine ten eleven")
-#     diary.add(entry1)
-#     diary.add(entry2)
-#     diary.add(entry3)
-#     extractor = ReadableEntryExtractor(diary)
-#     assert extractor.extract(2, 3) == ['entry1']
+'''When add multiple diary entries ReadableEntryExtractor extracts
+and returns a the longest readable'''
+def test_extracts_longest_multiple_enties():
+    diary = Diary()
+    entry1 = DiaryEntry("My title 1", "One two three four five")
+    entry2 = DiaryEntry("My title 2", "six seven eight")
+    entry3 = DiaryEntry("My title 3", "nine ten eleven")
+    diary.add(entry1)
+    diary.add(entry2)
+    diary.add(entry3)
+    extractor = ReadableEntryExtractor(diary)
+    assert extractor.extract(2, 3) == entry1
 
-# '''When add multiple diary entries ReadableEntryExtractor#extracts
-# and returns readable -> not enough time to read any, returns an ematy list'''
-# diary = Diary()
-# entry1 = DiaryEntry("My title 1", "One two three four five")
-# entry2 = DiaryEntry("My title 2", "six seven eight")
-# entry3 = DiaryEntry("My title 3", "nine ten eleven")
-# diary.add(entry1)
-# diary.add(entry2)
-# diary.add(entry3)
-# extractor = ReadableEntryExtractor(diary)
-# extractor.extract(1, 2) # => []
+'''When add multiple diary entries ReadableEntryExtractor extracts
+and returns a the longest readable'''
+def test_extracts_longest_multiple_enties_two():
+    diary = Diary()
+    entry1 = DiaryEntry("My title 1", "six seven eight")
+    entry2 = DiaryEntry("My title 2", "One two three four five")
+    entry3 = DiaryEntry("My title 3", "nine ten eleven")
+    diary.add(entry1)
+    diary.add(entry2)
+    diary.add(entry3)
+    extractor = ReadableEntryExtractor(diary)
+    assert extractor.extract(2, 3) == entry2
 
-# '''When add no diary entries ReadableEntryExtractor#extracts
-# and returns empty list'''
-# diary = Diary()
-# extractor = ReadableEntryExtractor(diary)
-# extractor.extract(1, 2) # => []
+'''When add multiple diary entries ReadableEntryExtractor#extracts
+and returns readable -> not enough time to read any, returns an ematy list'''
+def test_not_enough_time_to_read():
+    diary = Diary()
+    entry1 = DiaryEntry("My title 1", "One two three four five")
+    entry2 = DiaryEntry("My title 2", "six seven eight")
+    entry3 = DiaryEntry("My title 3", "nine ten eleven")
+    diary.add(entry1)
+    diary.add(entry2)
+    diary.add(entry3)
+    extractor = ReadableEntryExtractor(diary)
+    assert extractor.extract(1, 2) == None
+
+'''When add no diary entries ReadableEntryExtractor#extracts
+and returns empty list'''
+def test_no_entries_addded():
+    diary = Diary()
+    extractor = ReadableEntryExtractor(diary)
+    assert extractor.extract(1, 2) == None
